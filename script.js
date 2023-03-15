13ac4a5fb037d04d56453b8c5ff60d52c6dc5bd7
aj2000 <hi@ajaysharma.dev>
2023-03-15 19:12:59 +0530

    readme update

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const byteSize = (str) => {
+  // write your code here
+};
+
+// Do not change the code below
+const str = prompt("Enter some string.");
+alert(byteSize(str));
